e70eaa56d07913e76e65cc4c4cbd4342bef64d95
BarSalem <barsalem555@gmail.com>
2022-03-03 19:19:29 +0200

    make

--- server.js
+++ server.js
@@ -18,6 +18,4 @@ if(process.env.NODE_ENV==='production'){
     app.get('*',(req,res)=> res.sendFile(path.resolve(__dirname,'./','build','index.html')))
 }
 
-app.listen(5000,function(){
-    console.log("Server is on");
-});
+app.listen(process.env.PORT || 3000);
